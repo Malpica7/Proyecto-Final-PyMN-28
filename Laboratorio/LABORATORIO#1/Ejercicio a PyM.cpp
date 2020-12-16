bcca69132e7fbc400ea493afaf98db22f9757572
Malpica7 <76075105+Malpica7@users.noreply.github.com>
2020-12-15 19:44:43 -0800

    Add files via upload

---
+++ Laboratorio/LABORATORIO#1/Ejercicio a PyM.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+main ()
+{
+	float radio, pi, r, volumen, v;
+	printf ("dame el radio");
+	scanf("%f", & radio);
+	pi=3.1416;
+	r=(radio)*(radio)*(radio);
+	v=(1.333);
+	volumen=(v)*(pi)*(r);
+	printf("El volumen es %f", volumen);
+}
